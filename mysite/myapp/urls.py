b50c59c465c902fcfc8a993c2d9f1ce262794982
Kris <kristian_dilov@abv.bg>
2022-08-17 09:08:42 +0300

    inital commit

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -7,5 +7,6 @@ app_name = 'myapp'
 urlpatterns = [
     path('base/', views.base, name = 'base'),
     path('user_info/', views.user_info, name = 'user_info'),
+    path("just_form/", views.just_form, name = "just_form")
 
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from .models import Car
+from django.shortcuts import render, redirect
+from django.urls import reverse
 # Create your views here.
 
 from django.http import HttpResponse
@@ -16,13 +17,60 @@ def base(request):
     return render(request, "myapp/main.html", {'title': title, "cars_list": cars_list })
 
 def user_info(request):
-    userinfo = {"username": "Geralt", "country": "Kaera Mohren"}
 
-    context = {"userinfo": userinfo, "title": "User Info Page"}
-    context_no_title = {"userinfo": userinfo}
+    if request.method == 'GET':
+        
+        # print("request.GET ==>>", request.GET)
+        print("request.GET==>>", request.session.get('username', False))
+
+        if request.session.get('username', False):
+            userinfo = {
+                'username': request.session['username'],
+                'country': request.session['country']
+            }
+
+        else:
+            userinfo = False
+
+        context = {'userinfo': userinfo,
+                    'title': 'User Info Page'}
+        template = 'myapp/user_info.html'
+        return render(request, template, context)
+
+
+        # userinfo = {'username': request.GET.get('username'), #?username=Josh&country=USA
+        #         'country': request.GET.get('country'),
+        #        }
+        # context = {'userinfo': userinfo}
+
+        # template = 'myapp/user_info.html'
+
+        # return render(request, template, context)
+    
+    elif request.method == "POST":
+        return HttpResponse('POST request here')
+
+    # userinfo = {"username": "Geralt", "country": "Kaera Mohren"}
+
+    # context = {"userinfo": userinfo, "title": "User Info Page"}
+    # context_no_title = {"userinfo": userinfo}
     # return render(request, "myapp/user_info.html", context)
 
-    return render(request, "myapp/user_info.html", context_no_title)
+    # return render(request, "myapp/user_info.html", context_no_title)
 
 
+def just_form(request):
+    if request.method == "GET":
+        context = {'title': 'Form Page'}
+        template = 'myapp/just_form.html'
+        return render(request, template, context)
+
+    elif request.method == "POST":
+        print("request.POST==>>", request.POST)
+        username = request.POST.get('username')
+        country = request.POST.get('country')
+        request.session['username'] = username
+        request.session['country'] = country
+
+        return redirect(reverse('myapp:user_info'))
 
